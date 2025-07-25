1edeb7b4d1ec6aa8a5b7671fb5ae25c06e09b648
Sadegh Ranjbar <s.ranjbar@dataak.com>
2025-07-25 15:38:57 +0330

    feat: add server socket to main

---
+++ src/main/java/org/sadegh/Main.java
@@ -0,0 +1,32 @@
+package org.sadegh;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int port = 1212;
+        try(ServerSocket proxyServerSocket = new ServerSocket(port)) {
+            System.out.println("Server started on port " + port);
+            while(true){
+                try{
+                    Socket clientSocket = proxyServerSocket.accept();
+                    new Thread(new HttpsSocket(clientSocket)).start();
+
+                }catch (Exception e) {
+                    System.out.println("Error in main loop: " + e.getMessage());
+                }
+            }
+        }catch (IOException e) {
+            System.out.println("Error starting server on port " + port + ": " + e.getMessage());
+            throw new IOException("Failed to start server on port " + port, e);
+        }
+
+
+    }
+
+}
